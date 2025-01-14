640c812490857451dbbaa26706c24e502deb5456
Marine <marine.lamour@gmx.fr>
2025-01-14 10:59:48 +0100

    [DONE]user_auth_roles

---
+++ src/Form/ProductType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('description', TextareaType::class, ['label' => 'Description'])
+            ->add('type', TextType::class, ['label' => 'Type'])
+            ->add('image', FileType::class, [
+                'label' => 'Image (PNG, JPG, JPEG, GIF)',
+                'mapped' => false, // Not mapped to the entity because it's a file input
+                'required' => false,
+                'attr' => [
+                    'accept' => 'image/*', // This is the correct way to add the accept attribute
+                ],
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
