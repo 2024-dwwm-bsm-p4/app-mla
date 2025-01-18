09056a8e9ff2cdd62baad060898409906780b5ac
Marine <marine.lamour@gmx.fr>
2025-01-18 19:24:16 +0100

    [DONE] homepage+tests

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product1 = new Product();
+        $product1->setName('Pomme');
+        $product1->setCategory('Fruits');
+        $product1->setDescription('Une pomme fraîche et croquante issue de l’agriculture biologique.');
+        $product1->setType('Alimentaire');
+        $product1->setImage('public/uploads/images/67866d5fc1e39.jpg'); // Nom du fichier de l'image dans le dossier public/uploads
+        $manager->persist($product1);
+
+        $product2 = new Product();
+        $product2->setName('Carotte');
+        $product2->setCategory('Légumes');
+        $product2->setDescription('Des carottes bio au goût sucré et délicieux.');
+        $product2->setType('Alimentaire');
+        $product2->setImage('public/uploads/images/carotte.jpg'); // Nom du fichier de l'image
+        $manager->persist($product2);
 
         $manager->flush();
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      // ...
+      
     },
   },
   plugins: [require('@tailwindcss/forms')],
