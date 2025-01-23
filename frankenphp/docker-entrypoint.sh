db16838e6cf304f3aee5ad5ac2865cbce15e5598
Marine <marine.lamour@gmx.fr>
2025-01-23 10:23:05 +0100

    [modif_docker-entrypoint]

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -60,4 +60,6 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	echo 'PHP app ready!'
 fi
 
+php bin/console tailwind:build --minify
+php bin/console asset-map:compile
 exec docker-php-entrypoint "$@"
