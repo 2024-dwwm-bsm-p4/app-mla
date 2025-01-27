0656aca95dd764c585fdc8896eed98716fdd852a
Marine Lamour <135592547+LamourMarine@users.noreply.github.com>
2025-01-27 15:20:23 +0100

    Deploy (#46)
    * [modif_Dockerfile]
    
    * [modif_Dockerfile]
    
    * [modif_Dockerfile]
    
    * NewFeature
    
    * deploy
    
    * [modif_Dockerfile]
    
    * [modif_compose.prod]
    
    * [test_environnement_dev]
    
    * [modif_compose.prod]
    
    * [modif_docker-entrypoint]
    
    * [test_tailwind_prod]
    
    * [modif_postcss.confing.js]
    
    * [modif_app.css]
    
    * [modif_base.html.twig]
    
    * [ajout_config_tailwind]
    
    * [ajout_config_tailwind]
    
    * [ajout_config_tailwind]
    
    * [modif_Dockerfile]
    
    * [modif_Dockerfile]
    
    * [test_tailwind]
    
    * [test_tailwind]
    
    * [test_preprod]
    
    * [test_pre-prod]
    
    * [test_pre-prod]
    
    * [test_pre-prod]
    
    * [test_pre-prod]
    
    * [test_pre-prod]
    
    * [test_preprod]
    
    * Update Deploy to Pre-Prod
    
    suppression push
    
    * [test_preprod]
    
    * [test_deploy]
    
    * [test_preprod]
    
    * [test_preprod]
    
    * [test_preprod]
    
    * [test_preprod]
    
    * [test_home_page]
    
    * [ajout_balises_meta]
    
    * [viewport]
    
    * [test_preprod]
    
    * [add_symfonycast_tailwind.yaml]
    
    * [test_preprod]
    
    * [modif_tailwind]
    
    * [ajout_pgadmin_compose.prod]
    
    * [ajout_pgadmin_compose.prod]
    
    * [test_modif_services.yaml]
    
    * [test_modif_services.yaml]
    
    * [uploads=>.gitignore]
    
    * [uploads=>.gitignore]
    
    * [uploads=>.gitignore]
    
    * [modif_ProductController]
    
    * [UserFixture]

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -50,6 +50,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 		if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
 			php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
+			# php bin/console doctrine:fixtures:load 
 		fi
 	fi
 
@@ -59,4 +60,6 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	echo 'PHP app ready!'
 fi
 
+php bin/console tailwind:build --minify
+php bin/console asset-map:compile
 exec docker-php-entrypoint "$@"
